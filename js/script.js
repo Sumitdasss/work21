fd2c132ca7385aea41a37d4d90d1dc823b51c1d9
Sumit Das <sumit@example.com>
2026-01-07 13:04:08 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,46 @@
+var namtaString = "5";
+var nam = "This is java script language class 01";
+var nam1 = true;
+console.log( namtaString*6);
+console.log( nam);
+console.log( nam1);
+
+
+const pi = 3.14;
+console.log( pi);
+
+a =5;
+b =10;
+console.log( a+b);
+
+{
+    let a = 20;
+    let b = 30;
+    console.log( a+b);
+}
+{
+    var a = 20;
+    var b = 30;
+  
+}
+  console.log( a+b);
+
+
+   var abc = "Hello World";
+let def = 5;
+var hij =true;
+console.log(typeof abc);
+console.log(typeof def);
+console.log(typeof hij);
+
+let Number = 5;
+console.log(  Number + "" + "x" + "" + "1" + "" + "=" + "" + Number*1);
+console.log(  Number + "" + "x" + "" + "1" + "" + "=" + "" + Number*2);
+console.log(  Number + "" + "x" + "" + "1" + "" + "=" + "" + Number*3);
+console.log(  Number + "" + "x" + "" + "1" + "" + "=" + "" + Number*4);
+console.log(  Number + "" + "x" + "" + "1" + "" + "=" + "" + Number*5);
+console.log(  Number + "" + "x" + "" + "1" + "" + "=" + "" + Number*6);
+console.log(  Number + "" + "x" + "" + "1" + "" + "=" + "" + Number*7);
+console.log(  Number + "" + "x" + "" + "1" + "" + "=" + "" + Number*8);
+console.log(  Number + "" + "x" + "" + "1" + "" + "=" + "" + Number*9);
+console.log(  Number + "" + "x" + "" + "1" + "" + "=" + "" + Number*10);  
