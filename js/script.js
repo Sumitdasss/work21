f69af922e3d748142281650c4e6815b96e145e5d
Sumit Das <sumit@example.com>
2026-02-09 20:56:25 +0600

    add

--- js/script.js
+++ js/script.js
@@ -1,52 +1,3 @@
-// =======================================================
-
-var namtaString = "5";
-var nam = "This is java script language class 01";
-var nam1 = true;
-console.log( namtaString*6);
-console.log( nam);
-console.log( nam1);
-
-// =========================================================
-// =========================================================
-const pi = 3.14;
-console.log( pi);
-// =========================================================
-a =5;
-b =10;
-console.log( a+b);
-
-{
-    let a = 20;
-    let b = 30;
-    console.log( a+b);
-}
-{
-    var a = 20;
-    var b = 30;
-  
-}
-  console.log( a+b);
-
-
-   var abc = "Hello World";
-let def = 5;
-var hij =true;
-console.log(typeof abc);
-console.log(typeof def);
-console.log(typeof hij);
-
-let number = 5;
-console.log(  number + "" + "x" + "" + "1" + "" + "=" + "" + number*1);
-console.log(  number + "" + "x" + "" + "1" + "" + "=" + "" + number*2);
-console.log(  number + "" + "x" + "" + "1" + "" + "=" + "" + number*3);
-console.log(  number + "" + "x" + "" + "1" + "" + "=" + "" + number*4);
-console.log(  number + "" + "x" + "" + "1" + "" + "=" + "" + number*5);
-console.log(  number + "" + "x" + "" + "1" + "" + "=" + "" + number*6);
-console.log(  number + "" + "x" + "" + "1" + "" + "=" + "" + number*7);
-console.log(  number + "" + "x" + "" + "1" + "" + "=" + "" + number*8);
-console.log(  number + "" + "x" + "" + "1" + "" + "=" + "" + number*9);
-console.log(  number + "" + "x" + "" + "1" + "" + "=" + "" + number*10);  
 
 
 $('.slider').slick({
@@ -91,3 +42,177 @@ $('.slider4').slick({
   arrows:false
   
 });
+
+let accountBtn=document.getElementById("accountBtn")
+let loginPage=document.getElementById("loginPage")
+let Homepage=document.getElementById("Homepage")
+let Register=document.getElementById("Register")
+let Registerbtn=document.getElementById("Registerbtn")
+let loginbtn=document.getElementById("loginbtn")
+let homePagebtn=document.getElementById("homePagebtn")
+let shopeBtn=document.getElementById("shopeBtn")
+let shopPart=document.getElementById("shopPart")
+let btn1=document.getElementById("btn1")
+let btn2=document.getElementById("btn2")
+let btn3=document.getElementById("btn3")
+let fristPage=document.getElementById("fristPage")
+let secondPage=document.getElementById("secondPage")
+let thardePage=document.getElementById("thardePage")
+let listType=document.getElementById("listType")
+let gridType=document.getElementById("gridType")
+let gridBtn=document.getElementById("gridBtn")
+let listBtn=document.getElementById("listBtn")
+let pageNumber=document.getElementById("pageNumber")
+let pageNumbertwo=document.getElementById("pageNumbertwo")
+let listTypetwo=document.getElementById("listTypetwo")
+let listTypethree=document.getElementById("listTypethree")
+let btn4=document.getElementById("btn4")
+let btn5=document.getElementById("btn5")
+let btn6=document.getElementById("btn6")
+
+
+
+accountBtn.addEventListener("click",()=>{
+Homepage.classList.add("hidden")
+loginPage.classList.remove("hidden")
+shopPart.classList.add("hidden")
+
+})
+
+
+
+
+
+Registerbtn.addEventListener("click",()=>{
+Homepage.classList.add("hidden")
+loginPage.classList.add("hidden")
+Register.classList.remove("hidden")
+
+})
+
+
+
+
+loginbtn.addEventListener("click",()=>{
+Homepage.classList.add("hidden")
+loginPage.classList.remove("hidden")
+Register.classList.add("hidden")
+
+})
+
+
+
+
+
+
+homePagebtn.addEventListener("click",()=>{
+Homepage.classList.remove("hidden")
+loginPage.classList.add("hidden")
+Register.classList.add("hidden")
+shopPart.classList.add("hidden")
+
+})
+
+
+
+
+
+
+shopeBtn.addEventListener("click",()=>{
+Homepage.classList.add("hidden")
+loginPage.classList.add("hidden")
+Register.classList.add("hidden")
+shopPart.classList.remove("hidden")
+pageNumbertwo.classList.add("hidden")
+
+
+})
+
+
+
+
+
+btn1.addEventListener("click",()=>{
+fristPage.classList.remove("hidden")
+secondPage.classList.add("hidden")
+thardePage.classList.add("hidden")
+})
+
+
+
+
+btn2.addEventListener("click",()=>{
+fristPage.classList.add("hidden")
+thardePage.classList.add("hidden")
+secondPage.classList.remove("hidden")
+})
+
+
+
+
+
+
+
+
+btn3.addEventListener("click",()=>{
+fristPage.classList.add("hidden")
+thardePage.classList.remove("hidden")
+secondPage.classList.add("hidden")
+})
+
+
+
+
+
+
+
+
+gridBtn.addEventListener("click",()=>{
+listType.classList.add("hidden")
+gridType.classList.remove("hidden")
+secondPage.classList.add("hidden")
+pageNumber.classList.remove("hidden")
+pageNumbertwo.classList.add("hidden")
+
+})
+
+
+
+
+
+
+
+listBtn.addEventListener("click",()=>{
+gridType.classList.add("hidden")
+listType.classList.remove("hidden")
+secondPage.classList.add("hidden")
+pageNumber.classList.add("hidden")
+pageNumbertwo.classList.remove("hidden")
+
+})
+
+btn4.addEventListener("click",()=>{
+
+listType.classList.remove("hidden")
+listTypetwo.classList.add("hidden")
+listTypethree.classList.add("hidden")
+
+})
+btn5.addEventListener("click",()=>{
+
+listType.classList.add("hidden")
+listTypetwo.classList.remove("hidden")
+listTypethree.classList.add("hidden")
+
+})
+btn6.addEventListener("click",()=>{
+
+listType.classList.add("hidden")
+listTypetwo.classList.add("hidden")
+listTypethree.classList.remove("hidden")
+
+})
+
+
+
+
